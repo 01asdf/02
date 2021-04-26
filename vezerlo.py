f2151ca60d10fad638b28de815572c2948fe65aa
Bandi <totthbandus10@gmail.com>
2021-04-26 10:00:35 +0200

    forciklus a több futásért

--- vezerlo.py
+++ vezerlo.py
@@ -3,17 +3,18 @@ import federated_main
 import sys
 
 def main():
-    for a in adatok.data.adathalmazok:
-        adatok.data.image_initialization=True
-        for m in adatok.data.modellek:
-            adatok.data.model=m
-            adatok.data.dataset=a
-            sys.stdout=open("kiserlet_"+m+"_"+a+".txt",'w')
-            if adatok.data.image_initialization:
-                adatok.init()
+    for db in range(0,5):
+        for a in adatok.data.adathalmazok:
+            adatok.data.image_initialization=True
+            for m in adatok.data.modellek:
+                adatok.data.model=m
+                adatok.data.dataset=a
+                sys.stdout=open("kiserlet_"+m+"_"+a+"_"+str(db)+".txt",'w')
+                if adatok.data.image_initialization:
+                    adatok.init()
+                    federated_main.main()
+
                 federated_main.main()
 
-            federated_main.main()
-                            
 
 main()
